6b3c8496e34aabbdb3a90c434911a86f8fa02fe2
akhribabderahmane <la_Cakhrib@esi.dz>
2023-09-05 18:19:21 +0100

    change basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeContextProvider>
-    <BrowserRouter>
+    <BrowserRouter basename='/country-api'>
        <App />
     </BrowserRouter>
     </ThemeContextProvider>
